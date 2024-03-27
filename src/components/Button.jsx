c8b2647460d6c58f6a221b80db93d09d109e2e1e
datguyaymen <the.aymen.benzarti@gmail.com>
2024-03-27 14:39:36 +0100

    brainwave project setup

---
+++ src/components/Button.jsx
@@ -0,0 +1,23 @@
+import ButtonSvg from "../assets/svg/ButtonSvg";
+
+const Button = ({ className, href, children, px, white }) => {
+  const classes = `button relative infline-flex items-conter justify-center h-11 transition-colors 
+  hover:text-color-1 ${px || "px-7"} ${white ? "text-n-8" : "text-n-1"} ${
+    className || ""
+  }`;
+  const spanClasses = "relative z-10";
+  const renderButton = () => (
+    <button className={classes}>
+      <span className={spanClasses}>{children}</span>
+      {ButtonSvg(white)}
+    </button>
+  );
+  const renderLink = () => {
+    <a href={href} className={classes}>
+      <span className={spanClasses}>{children}</span>
+    </a>;
+  };
+  return href ? renderLink() : renderButton();
+};
+
+export default Button;
